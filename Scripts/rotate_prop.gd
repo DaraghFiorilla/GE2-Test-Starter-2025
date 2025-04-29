9a948390963811aefa993fb5d4e87db7cb47fcf9
XPS <daraghfiorillafpl@gmail.com>
2025-04-29 10:13:19 +0100

    rotating prop arms

--- Scripts/rotate_prop.gd
+++ Scripts/rotate_prop.gd
@@ -1,11 +1,18 @@
 extends Node3D
 
-@export var rotate_speed : float
-
-func rotate_toward():
-	
-	pass
+@export var rotate_force : float
+@export var rotate_increment : float
+@export var max_rotate_force : float
 
 func _physics_process(delta: float) -> void:
-	rotate_object_local(Vector3.LEFT, rotate_speed)
-	pass
+	if Input.is_key_pressed(KEY_SPACE):
+		if rotate_force < max_rotate_force:
+			rotate_force += rotate_increment
+		elif rotate_force > max_rotate_force:
+			rotate_force = max_rotate_force
+	else:
+		if rotate_force > 0:
+			rotate_force -= rotate_increment
+		elif rotate_force < 0:
+			rotate_force = 0
+	rotate_y(rotate_force)
